4b0e046f679ee15065af23899815d432e7d8d730
Lucy Lima <lucyatmosphere@gmail.com>
2024-03-03 10:16:39 -0300

    modal em progresso

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -1,10 +1,41 @@
-let container = document.getElementsByClassName('container-principal')[0]
-let semItems = document.getElementsByClassName('sem-items')[0]
-let menu = document.getElementsByClassName('menu-opcao')
+const container = document.getElementsByClassName('container-principal')[0]
+const semItems = document.getElementsByClassName('sem-items')[0]
+const menu = document.getElementsByClassName('menu-opcao')
+const adicionarNovo = document.getElementsByClassName('adicionar-novo')
+const modal = document.getElementsByClassName('modal-cadastro')[0]
 
 
+const mostrarModal = () => {
+  console.log('click')
+  modal.style.display = 'block'
+}
 
-document.addEventListener('DOMContentLoaded', () => { 
 
-})
+for (let item of adicionarNovo) {
+  item.addEventListener('click', mostrarModal)
+}
 
+for (let item of menu) {
+  item.addEventListener('click', () => {
+    switch (item.id) {
+      case 'inicio':
+
+        break;
+      case 'consultas':
+
+        break;
+        case 'conta':
+
+        break;
+        case 'pets':
+
+        break;
+    }
+  })
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
