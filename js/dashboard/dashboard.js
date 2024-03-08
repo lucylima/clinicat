023730d6f62ffad02258ea91aedc58d97407d361
lucylima <lucyatmosphere@gmail.com>
2024-03-08 17:38:27 -0300

    dashboard: criação do crud em andamento

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -10,36 +10,6 @@ const $especialidade = document.getElementsByClassName('especialidades')[0]
 const $nomeTutor = document.getElementById('nome')
 const $nomePet = document.getElementById('nomePet')
 
-const definirLocalStorage = (banco) => localStorage.setItem("banco_clinicat_pets", JSON.stringify(banco))
-const pegarLocalStorage = () => JSON.parse(localStorage.getItem('banco_clinicat_pets')) ?? []
-const lerBanco = () => pegarLocalStorage()
-
-const salvarPet = (pet) => {
-  const banco = pegarLocalStorage()
-  banco.push(pet)
-  definirLocalStorage(banco)
-}
-
-const editarPet = (id) => {
-  mostrarModal()
-  console.log(`Editando ${id}`)
-  console.log('click')
-}
-
-const deletarPet = (id) => {
-  const banco = lerBanco()
-  console.log(id.id)
-  banco.splice(id.id, 1)
-  console.log(banco)
-}
-
-const limparCampos = () => {
-  $nomeTutor.value = ''
-  $nomePet.value = ''
-  $especialidade.value = ''
-  for (let radio of $tipoAnimal) radio.checked = false
-  document.getElementById('nome').dataset.index = 'novo'
-}
 
 class Animal {
   constructor(id, nome, raca, especialidade, dono) {
@@ -52,97 +22,129 @@ class Animal {
 }
 
 
+const definirLocalStorage = (banco) => localStorage.setItem("banco_clinicat_pets", JSON.stringify(banco))
+const pegarLocalStorage = () => JSON.parse(localStorage.getItem('banco_clinicat_pets')) ?? []
+const lerBanco = () => pegarLocalStorage()
+
+const criaPet = () => {
+  const banco = pegarLocalStorage()
+  banco.push(pet)
+  definirLocalStorage(banco)
+}
+
+const deletarPet = (index) => {
+  const banco = lerBanco()
+  banco.splice(index, 1)
+  definirLocalStorage(banco)
+}
+
+const atualizarPet = (index, pet) => {
+  const banco = lerBanco()
+  banco[index] = pet
+  definirLocalStorage(banco)
+}
+
+const limparCampos = () => {
+  $nomeTutor.value = ''
+  $nomePet.value = ''
+  $especialidade.value = ''
+  for (let radio of $tipoAnimal) radio.checked = false
+  document.getElementById('nome').dataset.index = 'novo'
+}
+
+const salvarPet = () => {
+  const index = document.getElementById('nome').dataset.index
+  if (index == 'novo') {
+    let tipoAnimal
+
+    const tamanho = lerBanco()
+    const index = tamanho.length + 1
+  
+    for (let radio of $tipoAnimal) {
+      if (radio.checked) tipoAnimal = radio.value
+    }
+  
+    let novoPet = new Animal(
+      index,
+      $nomePet.value,
+      tipoAnimal,
+      $especialidade.value,
+      $nomeTutor.value
+    )
+    if (index === 'novo') {
+      criaPet(novoPet)
+      atualizarPet()
+      fecharModal()
+    } else {
+      atualizarPet(index, client)
+      atualizarPet()
+      fecharModal()
+    }
+  
+    // Toastify({
+    //   text: "Pet cadastrado com sucesso!",
+    //   duration: 4000,
+    //   style: {
+    //     background: "linear-gradient(25deg, rgba(106,102,242,1) 1%, rgba(124,120,247,1) 50%, rgba(156,153,255,1) 100%)",
+    //   }
+    // }).showToast();
+}
 
 $formularioCadastro.addEventListener('submit', e => {
   e.preventDefault()
 
-  let tipoAnimal
-
-  const tamanho = lerBanco()
-  const index = tamanho.length + 1
-
-  for (let radio of $tipoAnimal) {
-    if (radio.checked) tipoAnimal = radio.value
-  }
-
-  let novoPet = new Animal(
-    index,
-    $nomePet.value,
-    tipoAnimal,
-    $especialidade.value,
-    $nomeTutor.value
-  )
-  if (index === 'novo') {
-    salvarPet(novoPet)
-    limparCampos()
-    $modal.style.display = "none"
-  } else {
-
-  }
-  Toastify({
-    text: "Pet cadastrado com sucesso!",
-    duration: 4000,
-    style: {
-      background: "linear-gradient(25deg, rgba(106,102,242,1) 1%, rgba(124,120,247,1) 50%, rgba(156,153,255,1) 100%)",
-    }
-  }).showToast();
-  salvarPet(novoPet)
-  fecharModal()
-  limparCampos()
 })
 
-const criarCards = () => {
-  let listaDosPets = lerBanco()
-  listaDosPets.forEach((pet) => {
-    let $card = document.createElement('div')
-    let $cardImagem = document.createElement('img')
-    let $cardTitulo = document.createElement('h4')
-    let $cardSubtitulo = document.createElement('p')
-    let $cardEspecialidade = document.createElement('p')
-    let $cardAcoes = document.createElement('div')
-    let $botaoEditar = document.createElement('button')
-    let $botaoDeletar = document.createElement('button')
+const criarCards = (pet) => {
+  pet = {}
 
-    $card.classList.add('cardPet')
-    $cardImagem.classList.add('imagem-cardPet')
-    $cardTitulo.classList.add('titulo-cardPet')
-    $cardSubtitulo.classList.add('subtitulo-cardPet')
-    $cardEspecialidade.classList.add('especialidade-cardPet')
-    $cardAcoes.classList.add('botoes-cardPet')
-    $botaoEditar.classList.add('botao-editar-card')
-    $botaoDeletar.classList.add('botao-deletar-card')
+  let $card = document.createElement('div')
+  let $cardImagem = document.createElement('img')
+  let $cardTitulo = document.createElement('h4')
+  let $cardSubtitulo = document.createElement('p')
+  let $cardEspecialidade = document.createElement('p')
+  let $cardAcoes = document.createElement('div')
+  let $botaoEditar = document.createElement('button')
+  let $botaoDeletar = document.createElement('button')
 
-    switch (pet.raca) {
-      case 'Cachorro': $cardImagem.src = 'https://placedog.net/100/50?r'
-        break
-      case 'Gato': $cardImagem.src = 'http://placekitten.com/100/50'
-        break
-      case 'Silvestre': $cardImagem.src = 'https://placebear.com/100/50'
-        break
-    }
-    
+  $card.classList.add('cardPet')
+  $cardImagem.classList.add('imagem-cardPet')
+  $cardTitulo.classList.add('titulo-cardPet')
+  $cardSubtitulo.classList.add('subtitulo-cardPet')
+  $cardEspecialidade.classList.add('especialidade-cardPet')
+  $cardAcoes.classList.add('botoes-cardPet')
+  $botaoEditar.classList.add('botao-editar-card')
+  $botaoDeletar.classList.add('botao-deletar-card')
 
-    $cardTitulo.innerText = pet.nome
-    $cardSubtitulo.innerText = pet.raca
-    $cardEspecialidade.innerText = pet.especialidade
-    $botaoEditar.value = 'editar'
-    $botaoEditar.innerText = 'Editar'
-    $botaoDeletar.value = 'deletar'
-    $botaoDeletar.innerText = 'Deletar'
+  switch (pet.raca) {
+    case 'Cachorro': $cardImagem.src = 'https://placedog.net/100/50?r'
+      break
+    case 'Gato': $cardImagem.src = 'http://placekitten.com/100/50'
+      break
+    case 'Silvestre': $cardImagem.src = 'https://placebear.com/100/50'
+      break
+  }
 
-    $botaoEditar.addEventListener('click', () => { editarPet(pet) })
-    $botaoDeletar.addEventListener('click', () => { deletarPet(pet) })
+  $cardTitulo.innerText = pet.nome
+  $cardSubtitulo.innerText = pet.raca
+  $cardEspecialidade.innerText = pet.especialidade
+  $botaoEditar.value = 'editar'
+  $botaoEditar.innerText = 'Editar'
+  $botaoDeletar.value = 'deletar'
+  $botaoDeletar.innerText = 'Deletar'
+
+  $botaoEditar.addEventListener('click', () => { editarPet(pet.id) })
+  $botaoDeletar.addEventListener('click', () => { deletarPet(pet.id) })
 
 
-    $card.appendChild($cardImagem)
-    $card.appendChild($cardTitulo)
-    $card.appendChild($cardSubtitulo)
-    $card.appendChild($cardEspecialidade)
-    $cardAcoes.appendChild($botaoEditar)
-    $cardAcoes.appendChild($botaoDeletar)
-    $card.appendChild($cardAcoes)
-    $container.appendChild($card)
-  });
+  $card.appendChild($cardImagem)
+  $card.appendChild($cardTitulo)
+  $card.appendChild($cardSubtitulo)
+  $card.appendChild($cardEspecialidade)
+  $cardAcoes.appendChild($botaoEditar)
+  $cardAcoes.appendChild($botaoDeletar)
+  $card.appendChild($cardAcoes)
+  $container.appendChild($card)
 }
 
 for (let item of $menu) {
@@ -160,6 +162,5 @@ for (let item of $menu) {
 
 
 
-
-criarCards()
 console.log(lerBanco())
+
