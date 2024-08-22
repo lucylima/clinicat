e704327afb4f9d89f7efa58ec1487dc59e00bb6a
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-08-22 16:11:43 -0300

    feat: crud de usuário completo

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,36 +1,77 @@
 import { database } from '../database/database'
 
 const createUser = async (user) => {
-  const { name, email, password } = user
-  const newUser = await database.user.create({
-    data: {
-      user
-    }
-  })
-}
-
-const findUser = async (user) => {
-  const { username, email, password } = user
-  const loginAuth = await database.user.findUniqueOrThrow({
-    where: {
-      OR: [
-        {
-          username
-        },
-        {
-          email
-        }
-      ],
-      AND: {
-        password
+  try {
+    const { name, email, password } = user
+    const newUser = await database.user.create({
+      data: {
+        user
       }
-    }
-  })
-  if (loginAuth) {
-    return true
-  } else {
-    return 'senha ou usuário incorretos'
+    })
+    return 'Usuário criado com sucesso!'
+  } catch (error) {
+    return error
   }
 }
 
-export { findUser }
+const findUser = async (user) => {
+  try {
+    const { username, email, password } = user
+    const loginAuth = await database.user.findUniqueOrThrow({
+      where: {
+        OR: [
+          {
+            username
+          },
+          {
+            email
+          }
+        ],
+        AND: {
+          password
+        }
+      }
+    })
+    if (loginAuth) {
+      return true
+    } else {
+      return 'Senha ou usuário incorretos'
+    }
+  } catch (error) {
+    return error
+  }
+}
+
+const updateUser = async (user) => {
+  try {
+    const { id, username, email, password } = user
+    const updadteUser = await database.user.update({
+      where: id,
+      data: user
+    })
+    if (updatedUser) {
+      return 'Usuário editado com sucesso'
+    } else {
+      return 'Erro na edição do usuário'
+    }
+  } catch (error) {
+    return error
+  }
+}
+
+const deleteUser = async (id) => {
+  try {
+    const deletedUser = await database.user.delete({
+      where: id
+    })
+    if (deletedUser) {
+      return 'Usuário deletado com sucesso'
+    } else {
+      return 'Erro ao deletar usuário'
+    }
+  } catch (error) {
+    return error
+  }
+}
+
+export { createUser, findUser, updateUser, deleteUser }
