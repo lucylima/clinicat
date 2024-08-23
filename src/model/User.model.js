8fdbb205e137621a36101249a060a36d45337c78
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-08-23 16:26:52 -0300

    fix: classe de usuários editada

--- src/model/User.model.js
+++ src/model/User.model.js
@@ -1,14 +1,16 @@
 class User {
-  constructor(name, username, email, password) {
+  constructor(name, telephone, gender, username, email, password) {
     this.name = name
+    this.telephone = telephone
+    this.gender = gender
     this.username = username
     this.email = email
     this.password = password
   }
 }
 
-const newUser = (name, username, email, password) => {
-  let user = new User(name, username, email, password)
+const newUser = (name, telephone, gender, username, email, password) => {
+  const user = new User(name, telephone, gender, username, email, password)
   return user
 }
 
