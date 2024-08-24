ce4b7928a537a5bf47555fa1a18f67a5c92a55e4
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-08-23 23:15:23 -0300

    feat: CRUD usuário completo, rotas feitas

--- clinicat_backend/src/controller/user.controller.js
+++ clinicat_backend/src/controller/user.controller.js
@@ -1,22 +1,22 @@
+import { newUser } from '../model/User.model'
 import { database } from '../database/database.js'
 
-const createUser = async (user) => {
+const createUser = async (req, res) => {
   try {
-    const { name, username, email, password, telephone, gender } = user
-    await database.user.create({
-      data: {
-        user
-      }
+    const { name, username, email, password, telephone, gender } = req.body
+    const user = newUser(name, telephone, gender, username, email, password)
+    const createdUser = await database.user.create({
+      data: { user }
     })
-    return 'Usuário criado com sucesso!'
+    return res.status(201).json({ createdUser })
   } catch (error) {
-    return error
+    return res.status(400).json({ error })
   }
 }
 
-const findUser = async (user) => {
+const findUser = async (req, res) => {
   try {
-    const { username, email, password } = user
+    const { username, email, password } = req.body
     const loginAuth = await database.user.findUniqueOrThrow({
       where: {
         OR: [
@@ -33,44 +33,47 @@ const findUser = async (user) => {
       }
     })
     if (loginAuth) {
-      return true
+      return res.status(200).json({ loginAuth })
     } else {
-      return false
+      return res.status(400).json({ error: 'senha ou usuario incorretos' })
     }
   } catch (error) {
-    return error
+    return res.status(400).json({ error })
   }
 }
 
-const updateUser = async (user) => {
+const updateUser = async (req, res) => {
   try {
-    const { id, name, username, email, password, telephone, gender } = user
+    const { id } = req.params
+    const { name, username, email, password, telephone, gender } = req.body
+    const user = newUser(name, username, email, password, telephone, gender)
     const updatedUser = await database.user.update({
       where: id,
       data: user
     })
     if (updatedUser) {
-      return 'Usuário editado com sucesso'
+      return res.status(200).json({ user })
     } else {
-      return 'Erro na edição do usuário'
+      return res.status(404).json({ error: 'user not found' })
     }
   } catch (error) {
-    return error
+    return res.status(400).json({ error })
   }
 }
 
-const deleteUser = async (id) => {
+const deleteUser = async (req, res) => {
   try {
+    const { id } = req.params
     const deletedUser = await database.user.delete({
       where: id
     })
     if (deletedUser) {
-      return 'Usuário deletado com sucesso'
+      return res.status(200).json({ deleteUser })
     } else {
-      return 'Erro ao deletar usuário'
+      return res.status(404).json({ error: 'não foi possível achar usuário' })
     }
   } catch (error) {
-    return error
+    return res.status(400).json({ error })
   }
 }
 
